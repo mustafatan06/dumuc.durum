95c2ef4c7e1c4d16edb9019a0e9c9754668ce5df
mustafatan06 <mustafatan690@gmail.com>
2026-08-24 00:24:48 +0300

    dümüç web sitesi güncellendi

---
+++ menu-data.js
@@ -0,0 +1,184 @@
+let menuData = {
+    "tavuk-doner": {
+        name: "Dümüç",
+        ingredients: "Tavuk döner, yeşillik, domates, soğan, turşu",
+        image: "dumuc.jpg",
+        prices: {
+            "75 gram": { normal: "145 TL", dost: "245 TL", kanka: "470 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "175 TL", dost: "275 TL", kanka: "530 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "200 TL", dost: "300 TL", kanka: "580 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "hatay-dumuc": {
+        name: "Hatay Dümüç",
+        ingredients: "Tavuk döner, patates, tursu,  dümüç sos, hatay sos",
+        image: "hatay.jpg",
+        prices: {
+            "75 gram": { normal: "170 TL", dost: "270 TL", kanka: "520 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "210 TL", dost: "310 TL", kanka: "600 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "240 TL", dost: "340 TL", kanka: "660 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "zurna-dumuc": {
+        name: "Zurna Dümüç",
+        ingredients: "Tavuk döner, patates, tursu,  dümüç sos, sarımsaklı mayonez",
+        image: "zurna.jpg",
+        prices: {
+            "75 gram": { normal: "185 TL", dost: "285 TL", kanka: "550 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "215 TL", dost: "315 TL", kanka: "610 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "245 TL", dost: "345 TL", kanka: "670 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "iskenderun-dumuc": {
+        name: "İskenderun Dümüç",
+        ingredients: "Tavuk döner, soğan, domates, maydonoz, dümüç sos",
+        image: "iskenderun.jpg",
+        prices: {
+            "75 gram": { normal: "190 TL", dost: "290 TL", kanka: "560 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "225 TL", dost: "325 TL", kanka: "630 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "250 TL", dost: "350 TL", kanka: "680 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "ates-dumuc": {
+        name: "Ateş Dümüç",
+        ingredients: "Tavuk Döner, patates, soğan, turşu, domates, maydonoz, acılı dümüç sos, sarımsaklı mayonez",
+        image: "ates.jpg",
+        prices: {
+            "75 gram": { normal: "255 TL", dost: "355 TL", kanka: "690 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "205 TL", dost: "285 TL", kanka: "550 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "300 TL", dost: "400 TL", kanka: "780 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "yesilim-dumuc": {
+        name: "Yeşillim Dümüç",
+        ingredients: "Tavuk döner, aysberg, kırmızı kıvırcık, patates, domates, turşu, dümüç sos, sarımsaklı mayonez",
+        image: "yesilim.jpg",
+        prices: {
+            "75 gram": { normal: "240 TL", dost: "340 TL", kanka: "660 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "280 TL", dost: "380 TL", kanka: "740 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "315 TL", dost: "415 TL", kanka: "815 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "chedarli-dumuc": {
+        name: "Chedarlı Dümüç",
+        ingredients: "Tavuk döner, patates, yeşillik, turşu, chedar sos, sarımsaklı mayonez",
+        image: "chedarli.jpg",
+        prices: {
+            "75 gram": { normal: "265 TL", dost: "365 TL", kanka: "710 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "305 TL", dost: "405 TL", kanka: "790 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "340 TL", dost: "440 TL", kanka: "860 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "doritoslu-tako": {
+        name: "Doritoslu Tako Dümüç",
+        ingredients: "Tavuk döner, doritos, chedar sos, patates, turşu,   sarımsaklı mayonez",
+        image: "doritoslu.jpg",
+        prices: {
+            "75 gram": { normal: "270 TL", dost: "370 TL", kanka: "720 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "315 TL", dost: "415 TL", kanka: "810 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "350 TL", dost: "450 TL", kanka: "880 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "barbekulu-dumuc": {
+        name: "Barbekülü Dümüç",
+        ingredients: "Tavuk döner, patates, turşu,  barbekü sos, sarımsaklı mayonez",
+        image: "barbekulu.jpg",
+        prices: {
+            "75 gram": { normal: "175 TL", dost: "255 TL", kanka: "490 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "255 TL", dost: "355 TL", kanka: "690 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "335 TL", dost: "435 TL", kanka: "850 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "kasarli-dumuc": {
+        name: "Kaşarlı Dümüç",
+        ingredients: "Tavuk döner, patates, turşu ,eritme kaşar, dümüç sos, sarımsaklı mayonez",
+        image: "kasarli.jpg",
+        prices: {
+            "75 gram": { normal: "265 TL", dost: "365 TL", kanka: "710 TL", extra: "Patates+İçecek: 100 TL" },
+            "100 gram": { normal: "305 TL", dost: "405 TL", kanka: "560 TL", extra: "Patates+İçecek: 100 TL" },
+            "125 gram": { normal: "340 TL", dost: "440 TL", kanka: "860 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "dumuc-kofte": {
+        name: "Dümüç Köfte",
+        ingredients: "Köfte, patates, soğan, domates, maydonoz, turşu, dümüç sos, sarımsaklı mayonez",
+        image: "kofte.jpg",
+        prices: {
+            "100 gram": { normal: "365 TL", dost: "465 TL", kanka: "910 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "dumuc-ates-kofte": {
+        name: "Dümüç Ateş Köfte",
+        ingredients: "Köfte, patates, soğan, domates, maydonoz, turşu, dümüç ateş sos, sarımsaklı mayonez",
+        image: "ates-kofte.jpg",
+        prices: {
+            "100 gram": { normal: "365 TL", dost: "465 TL", kanka: "910 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "kofte-burger": {
+        name: "Dümüç Köfte Burger",
+        ingredients: "Burger köftesi,  soğan, domates,  turşu, cheddar peyniri, ketçap, sarımsaklı mayonez",
+        image: "burger.jpg",
+        prices: {
+            "100 gram": { normal: "330 TL", dost: "430 TL", kanka: "840 TL", extra: "Patates+İçecek: 80 TL" }
+        }
+    },
+    "double-burger": {
+        name: "Dümüç Double burger",
+        ingredients: "2 Burger köftesi,  soğan, domates,  turşu, cheddar peyniri, ketçap, sarımsaklı mayonez",
+        image: "double-burger.jpg",
+        prices: {
+            "200 gram": { normal: "460 TL", dost: "560 TL", kanka: "1100 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "citir-tavuk-burger": {
+        name: "Dümüç Çıtır Tavuk Burger",
+        ingredients: "Çıtır tavuklar,  soğan, domates,  turşu, cheddar peyniri, ketçap, sarımsaklı mayonez",
+        image: "citir-burger.jpg",
+        prices: {
+            "100 gram": { normal: "220 TL", dost: "320 TL", kanka: "620 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "dumuc-salata": {
+        name: "Dümüç Salata",
+        ingredients: "Aysberg, kırmızı kıvırcık, havuç, mısır, dümüç sos, çıtır tavuklar",
+        image: "salata.jpg",
+        prices: {
+            "120 gram": { normal: "280 TL", dost: "-", kanka: "-", extra: "Özel Soslu" }
+        }
+    },
+    "kumrum": {
+        name: "Kumrum",
+        ingredients: "Sosis, sucuk, kaşar, turşu,  ketçap, mayonez",
+        image: "kumru.jpg",
+        prices: {
+            "200 gram": { normal: "185 TL", dost: "285 TL", kanka: "550 TL", extra: "Patates+İçecek: 100 TL" }
+        }
+    },
+    "sogan-halkasi-4": { name: "Soğan halkası 4'lü", ingredients: "Atıştırmalık", image: "", prices: { "Tek": { normal: "50 TL", dost: "-", kanka: "-" } } },
+    "sogan-halkasi-8": { name: "Soğan halkası 8'li", ingredients: "Atıştırmalık", image: "", prices: { "Tek": { normal: "85 TL", dost: "-", kanka: "-" } } },
+    "baharatli-patates": { name: "Baharatlı Patates", ingredients: "Baharatlı patates kızartması", image: "", prices: { "Tek": { normal: "90 TL", dost: "-", kanka: "-" } } },
+    "citir-tavuk-4": { name: "Çıtır tavuk 4'lü", ingredients: "Çıtır tavuk parçaları", image: "", prices: { "Tek": { normal: "60 TL", dost: "-", kanka: "-" } } },
+    "citir-tavuk-8": { name: "Çıtır tavuk 8'li", ingredients: "Çıtır tavuk parçaları", image: "", prices: { "Tek": { normal: "100 TL", dost: "-", kanka: "-" } } },
+    "dumuc-baslangic": { name: "Dümüç başlangıç", ingredients: "Patates, 4li çıtır tavuk küpleri ,4 lü soğan halkası", image: "", prices: { "Tek": { normal: "200 TL", dost: "-", kanka: "-" } } },
+    "firinda-sutlac": { name: "Fırında Sütlaç", ingredients: "Tatlı", image: "", prices: { "Tek": { normal: "100 TL", dost: "-", kanka: "-" } } },
+    "pepsi": { name: "Pepsi", ingredients: "İçecek", image: "", prices: { "Tek": { normal: "90 TL", dost: "-", kanka: "-" } } },
+    "yedigun": { name: "Yedigün", ingredients: "İçecek", image: "", prices: { "Tek": { normal: "90 TL", dost: "-", kanka: "-" } } },
+    "sevenup": { name: "Sevenup", ingredients: "İçecek", image: "", prices: { "Tek": { normal: "90 TL", dost: "-", kanka: "-" } } },
+    "limonata": { name: "Limonata", ingredients: "İçecek", image: "", prices: { "Tek": { normal: "90 TL", dost: "-", kanka: "-" } } },
+    "ayran": { name: "Ayran", ingredients: "İçecek", image: "", prices: { "Küçük": { normal: "45 TL", dost: "-", kanka: "-" }, "Büyük": { normal: "70 TL", dost: "-", kanka: "-" } } },
+    "tropicana": { name: "Tropicana", ingredients: "İçecek", image: "", prices: { "Tek": { normal: "90 TL", dost: "-", kanka: "-" } } },
+    "salgam": { name: "Şalgam (acılı- acısız)", ingredients: "İçecek", image: "", prices: { "Tek": { normal: "65 TL", dost: "-", kanka: "-" } } },
+    "soda": { name: "Soda", ingredients: "İçecek", image: "", prices: { "Tek": { normal: "50 TL", dost: "-", kanka: "-" } } },
+    "su": { name: "Su", ingredients: "İçecek", image: "", prices: { "Tek": { normal: "30 TL", dost: "-", kanka: "-" } } }
+};
+
+function getMenuData() {
+    let saved = localStorage.getItem('dümüç_menu_data_v11');
+    if (saved) return JSON.parse(saved);
+    localStorage.setItem('dümüç_menu_data_v11', JSON.stringify(menuData));
+    return menuData;
+}
+function saveMenuData(data) {
+    localStorage.setItem('dümüç_menu_data_v11', JSON.stringify(data));
+}
